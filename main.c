99da2b8c75e70cc26de0257995de3533d57489f5
flo071 <flo.hintermeier@gmail.com>
2017-03-19 17:26:56 +0100

    clean up

--- main.c
+++ main.c
@@ -3,32 +3,17 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-
-#include "dht/dht.h"
 #include "dht/dht.c"
-
-#define UART_BAUD_RATE 2400
-#include "uart/uart.h"
-#include <uart/uart.c>
-
+#include "uart/uart.c"
 #include "lcd/LCD.c"
-
-int main(void)
-{
+#define UART_BAUD_RATE 2400
+int main(void){
     lcd_init();
-    /*while(1){
-    	lcd_setcursor(0,1);
-    	lcd_string("Hi!");
-    };*/
-
 	char printbuff[100];
-
 	//init uart
 	uart_init( UART_BAUD_SELECT(UART_BAUD_RATE,F_CPU) );
-
 	//init interrupt
 	sei();
-
 	#if DHT_FLOAT == 0
 	int8_t temperature = 0;
 	int8_t humidity = 0;
@@ -36,10 +21,8 @@ int main(void)
 	float temperature = 0;
 	float humidity = 0;
 	#endif
-
-	for (;;) {
-		if(dht_gettemperaturehumidity(&temperature, &humidity) != -1) {
-
+	while(1){
+		if(dht_gettemperaturehumidity(&temperature, &humidity)!=-1){
 			#if DHT_FLOAT == 0
 			itoa(temperature, printbuff, 10);
 			#elif DHT_FLOAT == 1
@@ -48,7 +31,6 @@ int main(void)
 			uart_puts("temperature: "); uart_puts(printbuff); uart_puts("C");uart_puts("\r\n");
 			lcd_setcursor(0,1);
 			lcd_string(printbuff);
-
 			#if DHT_FLOAT == 0
 			itoa(humidity, printbuff, 10);
 			#elif DHT_FLOAT == 1
@@ -57,11 +39,8 @@ int main(void)
 			uart_puts("humidity: "); uart_puts(printbuff); uart_puts("%RH");uart_puts("\r\n");
 			lcd_setcursor(0,2);
 			lcd_string(printbuff);
-
 		}
-
 		uart_puts("\r\n");
-
 		_delay_ms(1500);
 	}
 	return 0;
